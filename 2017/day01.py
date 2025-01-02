452587f950a2b1be54fd512eafe4e5d91cd5a2ce
Zyaad Jaunnoo <zyaad.jaunnoo@gmail.com>
2025-01-02 23:06:34 +0400

    Day01 solution of aoc 2017

--- 2017/day01.py
+++ 2017/day01.py
@@ -1,10 +1,14 @@
-def f(s, step):
+def solve(s, step):
     r = 0
     for i in range(len(s)-1, -1, -1):
         if s[i] == s[i-step]:
             r += int(s[i])
     return r
 
-s = input()
-print(f'Solution to part A = {f(s, 1)}')
-print(f'Solution to part A = {f(s, len(s) // 2)}')
+if __name__ == '__main__':
+    filename = './inputs/day01.in'
+    with open(filename, 'r') as f:
+        s = f.readline()
+
+    print(f'Solution to part A = {solve(s, 1)}')
+    print(f'Solution to part A = {solve(s, len(s) // 2)}')
